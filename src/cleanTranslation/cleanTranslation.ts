bc87a90dad37c04a598547253b9ee019f7aa2ff8
Maksim Iakovenko <yakovenkomax@protonmail.com>
2024-02-10 13:15:51 +0100

    Fix formatting before looking for duplicates

--- src/cleanTranslation/cleanTranslation.ts
+++ src/cleanTranslation/cleanTranslation.ts
@@ -4,15 +4,15 @@ import { removeUppercase } from 'cleanTranslation/removeUppercase';
 import { trimWhitespace } from 'cleanTranslation/trimWhitespace';
 
 export const cleanTranslation = (word: string, translation: Translation) => {
+  let cleanedTranslation = removeUppercase(word, translation);
+
+  cleanedTranslation = trimWhitespace(cleanedTranslation);
+
   // Remove the main translation from all frequencies to avoid duplication
-  let cleanedTranslation = removeWord(translation, translation.main);
+  cleanedTranslation = removeWord(cleanedTranslation, translation.main);
 
   // Repeat without the 'to ' in verbs
   cleanedTranslation = removeWord(cleanedTranslation, translation.main.replace('to ', ''));
-
-  cleanedTranslation = removeUppercase(word, cleanedTranslation);
-
-  cleanedTranslation = trimWhitespace(cleanedTranslation);
 
   return cleanedTranslation;
 };
